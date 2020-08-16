d3abacdf8768dc2064172f0d6214dcfbffc342a1
Josh Chough <joshchough@gmail.com>
2020-08-16 19:08:09 -0400

    Alert Context Workflow

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const alertState = useContext(AlertContext);
+  const { alert } = alertState;
+
   return (
     alert !== null && (
       <div className={`alert alert-${alert.type}`}>
